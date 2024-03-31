502edf8866aa239e43154f806d1370e7aaf47dd9
SwadheenMishra <144024958+SwadheenMishra@users.noreply.github.com>
2024-03-31 08:24:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from keras.models import load_model  # TensorFlow is required for Keras to work
-import cv2  # Install opencv-python
+from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 
 # Disable scientific notation for clarity
@@ -11,41 +11,33 @@ model = load_model("keras_Model.h5", compile=False)
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
 
-# CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(1)
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-while True:
-    # Grab the webcamera's image.
-    ret, image = camera.read()
+# Replace this with the path to your image
+image = Image.open("test.png").convert("RGB")
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+# resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+# turn the image into a numpy array
+image_array = np.asarray(image)
 
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # Normalize the image array
-    image = (image / 127.5) - 1
+# Load the image into the array
+data[0] = normalized_image_array
 
-    # Predicts the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+# Predicts the model
+prediction = model.predict(data)
+index = np.argmax(prediction)
+class_name = class_names[index]
+confidence_score = prediction[0][index]
 
-    # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
-    # Listen to the keyboard for presses.
-    keyboard_input = cv2.waitKey(1)
-
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
-        break
-
-camera.release()
-cv2.destroyAllWindows()
+# Print prediction and confidence score
+print("Class:", class_name[2:], end="")
+print("Confidence Score:", confidence_score)
